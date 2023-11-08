e83a2378320b5579c9d6479a2fcdb60d7549cba8
Aman Singh <142470207+aman-singh14@users.noreply.github.com>
2023-11-08 01:07:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/member/skills.html',
+    controller: 'SkillsController',
+    controllerAs: 'skillsCtrl'
+  };
+}
+    
